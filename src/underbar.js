2a28e4e24847e55dd5a4b7cff6ede97938d23f46
Kelly <kelly@groworganic.com>
2014-07-21 11:00:43 -0700

    Clever some method

--- src/underbar.js
+++ src/underbar.js
@@ -203,13 +203,13 @@ var _ = {};
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
-    // if (collection.length === 0) return false;
-    // iterator || (iterator = _.identity);
-    // return _.every(collection, function(isTrue, value){
-    //   if (iterator(isTrue)) return true;
-    //   else return false;
-    // });
-  };
+    if (collection.length === 0) return false;
+    iterator || (iterator = _.identity);
+
+    return !_.every(collection, function(value){
+      return !iterator(value);
+    });
+ };
 
 
   /**
