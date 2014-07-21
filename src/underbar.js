3d4611dea8316cfb41a96eb7ced8a6cd2048bb7e
Kelly <kelly@groworganic.com>
2014-07-21 11:37:33 -0700

    Extend helper

--- src/underbar.js
+++ src/underbar.js
@@ -231,7 +231,15 @@ var _ = {};
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    var orgObj = obj;
+    _.each(arguments, function(obj) {
+        for (var key in obj) {
+          orgObj[key] = obj[key];
+        }
+    });
+    return orgObj;
   };
+
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
